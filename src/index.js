79a2a7e8a7e0e15dcf163463ac6171f6f7b0a6c6
chrystalme <afam_ifeanyi@live.com>
2021-05-12 13:18:10 +0300

    Resolve async errors

--- src/index.js
+++ src/index.js
@@ -32,35 +32,28 @@ function displayRemove() {
   resultInner.innerHTML = '';
 }
 
-function getData() {
-  const requestUrl = url;
-  const request = new XMLHttpRequest();
-  request.open('GET', requestUrl);
-  request.responseType = 'json';
-  request.send();
-  
-  request.onload = () => {
-    const data = request.response;
-    h3.innerHTML = `${data.name}`;
-    resultInner.appendChild(h3);
-    temp.innerHTML = Math.round(data.main.temp);
-    tempUnit.innerHTML = 'C';
-    p.innerHTML = `Feels Like : ${Math.round(data.main.feels_like)}`;
-    p1.innerHTML = `Minimum Temperature : ${Math.round(data.main.temp_min)}`;
-    p2.innerHTML = `Maximum Temperature : ${Math.round(data.main.temp_max)}`;
-    p3.innerHTML = `Humidity : ${Math.round(data.main.humidity)}`;
-    temp.appendChild(tempUnit);
-    resultInner.appendChild(temp);
-    resultInner.appendChild(p);
-    resultInner.appendChild(p1);
-    resultInner.appendChild(p2);
-    resultInner.appendChild(p3);
-    result.appendChild(resultInner);
-  };
+async function getData(url) {
+  const response = await fetch(url, { mode: 'cors' });
+  const data = await response.json();
+  h3.innerHTML = `${data.name}`;
+  resultInner.appendChild(h3);
+  temp.innerHTML = Math.round(data.main.temp);
+  tempUnit.innerHTML = 'C';
+  p.innerHTML = `Feels Like : ${Math.round(data.main.feels_like)}`;
+  p1.innerHTML = `Minimum Temperature : ${Math.round(data.main.temp_min)}`;
+  p2.innerHTML = `Maximum Temperature : ${Math.round(data.main.temp_max)}`;
+  p3.innerHTML = `Humidity : ${Math.round(data.main.humidity)}`;
+  temp.appendChild(tempUnit);
+  resultInner.appendChild(temp);
+  resultInner.appendChild(p);
+  resultInner.appendChild(p1);
+  resultInner.appendChild(p2);
+  resultInner.appendChild(p3);
+  result.appendChild(resultInner);
 }
 
 url = 'https://api.openweathermap.org/data/2.5/weather?q=jos&units=metric&appid=f143eabdcb81c14621c46bee170ba541';
-getData();
+getData(url);
 
 btnGetWeather.addEventListener('click', () => {
   displayRemove();
@@ -75,32 +68,7 @@ btnGetWeather.addEventListener('click', () => {
   p.classList.remove('text-danger');
   url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=f143eabdcb81c14621c46bee170ba541`;
 
-  // async function getData() {
-  //   const response = await fetch(url,
-  //     {
-  // mode: 'cors';
-  //   } );
 
-  //   if (response.status === 400) {
-  //     return
-  //   }else{
-  //     const data = await response.json();
-  //     console.log(data);
-  //   }
-  // }
-
-  // fetch(url)
-  //   .then((response) => response.json()).then((response) => {
-  //     const data = response;
-  //     if (data.city) {
-  //       console.log(data.city.name);
-  //     }else{
-  //       const p = document.createElement('p');
-  //       p.innerHTML = 'City does not exist.';
-  //       myCity.appendChild(p);
-  //     }
-  //   });
-
-  getData();
+  getData(url);
   reset();
 });
