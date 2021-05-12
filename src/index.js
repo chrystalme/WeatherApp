a110541fdd87dfc7788cf7e11c59a5739c22e215
chrystalme <afam_ifeanyi@live.com>
2021-05-12 19:53:22 +0300

    Add weather description

--- src/index.js
+++ src/index.js
@@ -19,10 +19,11 @@ const p3 = document.createElement('p');
 h3.classList.add('my-4');
 temp.style.fontSize = '6rem';
 temp.style.position = 'relative';
-tempUnit.classList.add('units');
+tempUnit.classList.add('units', 'btn-group');
+tempUnit.role = 'group';
 tempUnit.style.position = 'absolute';
 tempUnit.style.top = '5px';
-tempUnit.style.right = '-25px';
+tempUnit.style.right = '25px';
 
 
 let url;
@@ -39,29 +40,44 @@ const displayRemove = () => {
 
 const getData = async url => {
   const response = await fetch(url, { mode: 'cors' });
+  if (response.status === 404) {
+    resultInner.classList.add('text-danger');
+    resultInner.classList.remove('text-white');
+    resultInner.innerHTML = 'City not found';
+    return;
+  }
+  resultInner.classList.remove('text-danger');
+  resultInner.classList.add('text-white');
   const data = await response.json();
-  h3.innerHTML = `${data.name}`;
+
+  h3.innerHTML = `${data.name} : ${data.weather[0].description}`;
   const iconImg = data.weather[0].icon;
   resultInner.appendChild(h3);
   temp.innerHTML = Math.round(data.main.temp);
-  tempUnit.innerHTML = "<span id='celsius'>C</span> | <span id='fahrenheit'>F</span>";
+  tempUnit.innerHTML = '<div id="celsius"><button type="button" class="btn btn-primary">C</button></div><button id="fahrenheit" type="button" class="btn btn-primary">F</button>';
   p.innerHTML = `Feels Like : ${Math.round(data.main.feels_like)}`;
   p1.innerHTML = `Minimum Temperature : ${Math.round(data.main.temp_min)}`;
   p2.innerHTML = `Maximum Temperature : ${Math.round(data.main.temp_max)}`;
   p3.innerHTML = `Humidity : ${Math.round(data.main.humidity)}`;
   icon.src = `http://openweathermap.org/img/wn/${iconImg}@2x.png`;
-  temp.appendChild(tempUnit);
   temp.appendChild(icon);
   resultInner.appendChild(temp);
   resultInner.appendChild(p);
   resultInner.appendChild(p1);
   resultInner.appendChild(p2);
   resultInner.appendChild(p3);
+  result.appendChild(tempUnit);
   result.appendChild(resultInner);
 };
 
 url = 'https://api.openweathermap.org/data/2.5/weather?q=jos&units=metric&appid=f143eabdcb81c14621c46bee170ba541';
 getData(url);
+
+const celsius = document.getElementById('celsius');
+celsius.addEventListener('click', () => {
+  console.log(celsius.innerHTML);
+  // document.getElementById('celsius').disabled = true;
+});
 
 btnGetWeather.addEventListener('click', () => {
   displayRemove();
@@ -74,16 +90,10 @@ btnGetWeather.addEventListener('click', () => {
   }
   city = myCity.value;
   p.classList.remove('text-danger');
-  url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=f143eabdcb81c14621c46bee170ba541`;
+  url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=f143eabdcb81c14621c46bee170ba541`;
 
 
   getData(url);
   reset();
 });
 
-tempUnit.addEventListener('click', () => {
-  const fahrenheit = document.getElementById('fahrenheit');
-  const celsius = document.getElementById('celsius');
-  fahrenheit.classList.add('text-bold');
-  celsius.classList.add('text-dark');
-});
